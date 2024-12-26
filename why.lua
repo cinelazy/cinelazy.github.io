9fd4ee90450089a7448c4c5bb1ffb4ffcef9176b
Dark <109311087+cinelazy@users.noreply.github.com>
2024-12-26 18:50:45 -0300

    Update why.lua

--- why.lua
+++ why.lua
@@ -1,4 +1,14 @@
+local yieldLibrary = {}
+local Utility = {}
 
-print("antes de dar valor")
+function yieldLibrary:CreateLib(LibraryConfig)
+	LibraryConfig = LibraryConfig or {}
+	LibraryConfig.Name = LibraryConfig.Name or "Custom Admin Library"
+	LibraryConfig.Version = LibraryConfig.Version or "v1.0.0"
 
-print("despues")
+	COREGUI = cloneref(game:GetService("CoreGui"))
+	Players = cloneref(game:GetService("Players"))
+    print(LibraryConfig.Name)
+    print(LibraryConfig.Version)
+end
+return yieldLibrary
